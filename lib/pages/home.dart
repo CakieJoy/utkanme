8ccd8992bcddea40b47e77dc74f397c8d28de1e7
BirthdayCake35 <utku0iletisim@gmail.com>
2025-03-03 18:38:45 +0300

    New: Home Page designed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cosmos/cosmos.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -67,43 +66,62 @@ class HomePage extends StatelessWidget {
           scrollable: true,
           scrollDirection: Axis.vertical,
           children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Merhabalar ben Utkan Aydın Junior Flutter Android Developer'ım.",
-                        style: GoogleFonts.poppins(fontSize: 25),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text("Github Projelerim için"),
-                          SizedBox(
-                            width: 5,
+            Center(
+              child: Container(
+                width: 500,
+                height: 300,
+                color: const Color.fromARGB(49, 158, 158, 158),
+                padding:
+                    EdgeInsets.all(20), // Padding'i Container'a uyguluyoruz
+                child: Column(
+                  // CosmosBody yerine Column kullanıyoruz
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Metni sola yaslamak için
+                  children: [
+                    SizedBox(height: 10), // SizedBox'ı Column'un içine alıyoruz
+                    Text("Utkan Aydın",
+                        style: GoogleFonts.poppins(
+                            fontSize: 30, fontWeight: FontWeight.bold)),
+                    Text("CakieJoy olarakda bilinir.",
+                        style: GoogleFonts.poppins(
+                            fontSize: 10, fontWeight: FontWeight.w100)),
+                    Text("Flutter Android Developer",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 22, fontWeight: FontWeight.w400)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () async {
+                            await launch("mailto:utkan@utkan.net");
+                          },
+                          child: Icon(
+                            Icons.email,
+                            color: Colors.deepOrange,
                           ),
+                        ),
+                      ],
+                    ),
+                    Center(
+                      child: Column(
+                        children: [
                           InkWell(
-                              onTap: () async {
-                                await launch(
-                                    "https://github.com/CakieJoy?tab=repositories");
-                              },
-                              child: Image.network(
-                                "https://ouch-cdn2.icons8.com/_qv0GZ-TvUNz7L6kTDY6jQTK0ZL8PBgeSRketqzOyB0/rs:fit:456:456/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNDEw/LzY2ZThhYzc1LTJh/ZjAtNDk4MC1hNThl/LWMwOWY0NWIyM2Mz/NS5wbmc.png",
-                                width: 20,
-                                height: 20,
-                              ))
+                            onTap: () async {
+                              await launch(
+                                  "https://github.com/cakiejoy/utkanme");
+                            },
+                            child: Text("Bu Site GitHub'da bulunuyor!",
+                                style: GoogleFonts.montserrat(
+                                    fontSize: 20, fontWeight: FontWeight.w800)),
+                          ),
                         ],
                       ),
-                    ],
-                  ),
-                )
-              ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ));
